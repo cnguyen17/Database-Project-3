362bad453740987802f80d64114b256ed12ec2e2
cnguyen17 <55762136+cnguyen17@users.noreply.github.com>
2021-04-19 17:58:26 -0600

    Add files via upload

---
+++ Database_project3/src/main/java/Course.java
@@ -0,0 +1,15 @@
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/*
+ * CS3810 - Principles of Database Systems - Spring 2021
+ * Instructor: Thyago Mota
+ * Description: DB 03 - Student
+ * Student(s) Name(s):Calvin Nguyen and Echglene Woy
+ */
+
+public class Course {
+
+
+}
---
+++ Database_project3/src/main/java/Enrollment.java
@@ -0,0 +1,11 @@
+/*
+ * CS3810 - Principles of Database Systems - Spring 2021
+ * Instructor: Thyago Mota
+ * Description: DB 03 - Enrollment
+ * Student(s) Name(s): Calvin Nguyen and Echglene Woy
+ */
+
+public class Enrollment {
+
+
+}
